26c013e4f1176c67f29148da8ac728ee5a4f1dfc
Alexey Smirnov <alexey.work@tut.by>
2021-11-23 22:16:48 +0300

    add key to tag br in BR2JSX

--- BR2JSX/components/BR2JSX.js
+++ BR2JSX/components/BR2JSX.js
@@ -6,7 +6,7 @@ import './BR2JSX.css';
 
 const regex = /(<br\s*\/*>)/;
 
-const BR2JSX = ( props ) => <div className='BR2JSX'>{props.text.split(regex).map(str => str.match(regex) ? <br /> : str)}</div>;
+const BR2JSX = ( props ) => <div className='BR2JSX'>{props.text.split(regex).map((str,i) => str.match(regex) ? <br key={i}/> : str)}</div>;
 
 BR2JSX.propTypes = {
   text: PropTypes.string.isRequired
